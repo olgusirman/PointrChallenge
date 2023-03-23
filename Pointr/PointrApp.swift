00a0c5b9a621ada752933316b63ca22f196e8b2f
Olgu SIRMAN <olgu.sirman@costacoffee.com>
2023-03-23 14:55:39 +0000

    Initial Commit

---
+++ Pointr/PointrApp.swift
@@ -0,0 +1,17 @@
+//
+//  PointrApp.swift
+//  Pointr
+//
+//  Created by Olgu SIRMAN on 23/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct PointrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
